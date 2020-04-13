45445f599c6f28973c95103d2507f6a3322fe251
Yashar kavosian <kavoosian@hotmail.com>
2020-04-13 11:27:16 +0430

    The Task Manager is complete

---
+++ miniTaskManager/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace miniTaskManager
+{
+    partial class frmTaskManger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpbRunningProcesses = new System.Windows.Forms.GroupBox();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEndProcess = new System.Windows.Forms.Button();
+            this.menuListBox = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.endTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrTaskMgr = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitTaskManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shutDownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turnOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.standByToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hibernateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.grpbRunningProcesses.SuspendLayout();
+            this.menuListBox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpbRunningProcesses
+            // 
+            this.grpbRunningProcesses.Controls.Add(this.lstTasks);
+            this.grpbRunningProcesses.Location = new System.Drawing.Point(12, 27);
+            this.grpbRunningProcesses.Name = "grpbRunningProcesses";
+            this.grpbRunningProcesses.Size = new System.Drawing.Size(365, 220);
+            this.grpbRunningProcesses.TabIndex = 0;
+            this.grpbRunningProcesses.TabStop = false;
+            this.grpbRunningProcesses.Text = "Running Processes";
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.ContextMenuStrip = this.menuListBox;
+            this.lstTasks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 29;
+            this.lstTasks.Location = new System.Drawing.Point(3, 33);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(359, 184);
+            this.lstTasks.TabIndex = 0;
+            this.lstTasks.Click += new System.EventHandler(this.lstTasks_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Run Process:";
+            // 
+            // btnEndProcess
+            // 
+            this.btnEndProcess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEndProcess.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEndProcess.Location = new System.Drawing.Point(264, 309);
+            this.btnEndProcess.Name = "btnEndProcess";
+            this.btnEndProcess.Size = new System.Drawing.Size(110, 45);
+            this.btnEndProcess.TabIndex = 2;
+            this.btnEndProcess.Text = "&End Process";
+            this.btnEndProcess.UseVisualStyleBackColor = true;
+            this.btnEndProcess.Click += new System.EventHandler(this.btnEndProcess_Click);
+            // 
+            // menuListBox
+            // 
+            this.menuListBox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.propertiesToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.endTaskToolStripMenuItem});
+            this.menuListBox.Name = "menuListBox";
+            this.menuListBox.Size = new System.Drawing.Size(138, 54);
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.propertiesToolStripMenuItem.Text = "&Properties";
+            this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.propertiesToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(134, 6);
+            // 
+            // endTaskToolStripMenuItem
+            // 
+            this.endTaskToolStripMenuItem.Name = "endTaskToolStripMenuItem";
+            this.endTaskToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.endTaskToolStripMenuItem.Text = "&End Process";
+            this.endTaskToolStripMenuItem.Click += new System.EventHandler(this.endTaskToolStripMenuItem_Click);
+            // 
+            // tmrTaskMgr
+            // 
+            this.tmrTaskMgr.Enabled = true;
+            this.tmrTaskMgr.Tick += new System.EventHandler(this.tmrTaskMgr_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.shutDownToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshNowToolStripMenuItem,
+            this.propertiesToolStripMenuItem1,
+            this.toolStripMenuItem1,
+            this.exitTaskManagerToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // refreshNowToolStripMenuItem
+            // 
+            this.refreshNowToolStripMenuItem.Name = "refreshNowToolStripMenuItem";
+            this.refreshNowToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.refreshNowToolStripMenuItem.Text = "&Refresh now";
+            // 
+            // propertiesToolStripMenuItem1
+            // 
+            this.propertiesToolStripMenuItem1.Name = "propertiesToolStripMenuItem1";
+            this.propertiesToolStripMenuItem1.Size = new System.Drawing.Size(168, 22);
+            this.propertiesToolStripMenuItem1.Text = "&Properties";
+            this.propertiesToolStripMenuItem1.Click += new System.EventHandler(this.propertiesToolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(165, 6);
+            // 
+            // exitTaskManagerToolStripMenuItem
+            // 
+            this.exitTaskManagerToolStripMenuItem.Name = "exitTaskManagerToolStripMenuItem";
+            this.exitTaskManagerToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.exitTaskManagerToolStripMenuItem.Text = "E&xit Task Manager";
+            this.exitTaskManagerToolStripMenuItem.Click += new System.EventHandler(this.exitTaskManagerToolStripMenuItem_Click);
+            // 
+            // shutDownToolStripMenuItem
+            // 
+            this.shutDownToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.turnOffToolStripMenuItem,
+            this.restartToolStripMenuItem,
+            this.standByToolStripMenuItem,
+            this.logOffToolStripMenuItem,
+            this.hibernateToolStripMenuItem});
+            this.shutDownToolStripMenuItem.Name = "shutDownToolStripMenuItem";
+            this.shutDownToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.shutDownToolStripMenuItem.Text = "&Shut down";
+            // 
+            // turnOffToolStripMenuItem
+            // 
+            this.turnOffToolStripMenuItem.Name = "turnOffToolStripMenuItem";
+            this.turnOffToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.turnOffToolStripMenuItem.Text = "&Turn Off";
+            this.turnOffToolStripMenuItem.Click += new System.EventHandler(this.turnOffToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.restartToolStripMenuItem.Text = "&Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // standByToolStripMenuItem
+            // 
+            this.standByToolStripMenuItem.Name = "standByToolStripMenuItem";
+            this.standByToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.standByToolStripMenuItem.Text = "&Stand By";
+            this.standByToolStripMenuItem.Click += new System.EventHandler(this.standByToolStripMenuItem_Click);
+            // 
+            // logOffToolStripMenuItem
+            // 
+            this.logOffToolStripMenuItem.Name = "logOffToolStripMenuItem";
+            this.logOffToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.logOffToolStripMenuItem.Text = "&Log Off";
+            this.logOffToolStripMenuItem.Click += new System.EventHandler(this.logOffToolStripMenuItem_Click);
+            // 
+            // hibernateToolStripMenuItem
+            // 
+            this.hibernateToolStripMenuItem.Name = "hibernateToolStripMenuItem";
+            this.hibernateToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.hibernateToolStripMenuItem.Text = "&Hibernate";
+            this.hibernateToolStripMenuItem.Click += new System.EventHandler(this.hibernateToolStripMenuItem_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Location = new System.Drawing.Point(264, 360);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(113, 45);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // frmTaskManger
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnEndProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grpbRunningProcesses);
+            this.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmTaskManger";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Task manager";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmTaskManger_Load);
+            this.grpbRunningProcesses.ResumeLayout(false);
+            this.menuListBox.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpbRunningProcesses;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEndProcess;
+        private System.Windows.Forms.ContextMenuStrip menuListBox;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem endTaskToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitTaskManagerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shutDownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turnOffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem standByToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hibernateToolStripMenuItem;
+        public System.Windows.Forms.Timer tmrTaskMgr;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ miniTaskManager/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+namespace miniTaskManager
+{
+    public partial class frmTaskManger : Form
+    {
+        [DllImport("powrorof.dll")]
+        public static extern Int32 SetSuspendState(Int32 HiberNate, Int32 Foce, Int32 Disable);
+        [DllImport("user32.dll")]
+        public static extern int ExitWindowsEX(int uFlags, int dwReason);
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+
+        public frmTaskManger()
+        {
+            InitializeComponent();
+        }//frmTaskManger
+        //-----------------------------------------------------------------------------------------
+        private void frmTaskManger_Load(object sender, EventArgs e)
+        {
+
+        }//frmTaskManger_Load
+        //-----------------------------------------------------------------------------------------
+        private void turnOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("Shutdown", "-s -t 00");
+        }//turnOffToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("Shutdown", "-r -t 00");
+        }//restartToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void standByToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.SetSuspendState(PowerState.Suspend, true, true);
+        }//standByToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void logOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExitWindowsEX(4, 0);
+        }//logOffToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void hibernateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.SetSuspendState(PowerState.Hibernate, true, true);
+        }//hibernateToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void tmrTaskMgr_Tick(object sender, EventArgs e)
+        {
+            Load_Task_List();
+        }//tmrTaskMgr_Tick
+
+        private void Load_Task_List()
+        {
+            Process[] allp = Process.GetProcesses();
+            if (lstTasks.Items.Count != allp.Length)
+            {
+                lstTasks.Items.Clear();
+                foreach (Process p in allp)
+                {
+                    lstTasks.Items.Add(p.ProcessName);
+                }//foreach
+                 // lstTasks.SetSelected(0, true);
+                label1.Text = "Running processes count=" + lstTasks.Items.Count.ToString();
+            }//if
+        }//Load_Task_List
+
+        //-----------------------------------------------------------------------------------------
+        private void exitTaskManagerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }//exitTaskManagerToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void btnEndProcess_Click(object sender, EventArgs e)
+        {
+            string strMesage = "";
+            string strTitle = "Mini Task Manager";
+            try
+            {
+                Process[] allp = Process.GetProcesses();
+                foreach (var p in allp)
+                {
+                    if (lstTasks.SelectedItem.ToString().ToLower() == p.ProcessName.ToLower())
+                    {
+                        strMesage = string.Format("Do you want to end the {0} application", p.ProcessName);
+                        DialogResult res = MessageBox.Show(strMesage, strTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (res == DialogResult.Yes)
+                        {
+                            p.Kill();
+                        }//if Yes is clicked
+                    }//if
+                }//foreach
+            }//try
+            catch (Exception ex)
+            {
+                strMesage = string.Format("Exception No 1. has occured , please try again or inform system administrator \n {0}", ex.Message);
+                MessageBox.Show(strMesage, strTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }//catch
+        }//btnEndProcess_Click
+        //-----------------------------------------------------------------------------------------
+        private void endTaskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }//endTaskToolStripMenuItem_Click
+        //-----------------------------------------------------------------------------------------
+        private void lstTasks_Click(object sender, EventArgs e)
+        {
+            string strTitle = "Mini Task Manager";
+            string strMessage = "";
+            ToolTip listB = new ToolTip();
+            listB.ToolTipTitle = "Information of selected process";
+            listB.ToolTipIcon = ToolTipIcon.Info;
+            listB.AutomaticDelay = 50;
+            listB.AutoPopDelay = 15000;
+            listB.UseAnimation = true;
+            
+
+            try
+            {
+                Process[] allp = Process.GetProcesses();
+                foreach (Process p in allp)
+                {
+
+                    FileInfo a = new FileInfo(p.MainModule.FileName);
+                    string len = null;
+                    len = a.Length.ToString() + "Bytes";                    
+                    strMessage += "Process name:" + p.ProcessName;
+                    strMessage += "\n";
+                    strMessage += "Machine Name:" + p.MachineName;
+                    strMessage += "\n";
+                    strMessage += "Memory Usage:" + p.WorkingSet64.ToString() + "KB";
+                    strMessage += "\n";
+                    strMessage += "Process ID:" + p.Id.ToString();
+                    strMessage += "\n";
+                    strMessage += "Address:" + p.MainModule.FileName;
+                    strMessage += "\n";
+                    strMessage += "File Name:" + p.MainModule.FileVersionInfo.OriginalFilename;
+                    strMessage += "\n";
+                    strMessage += "File Version:" + p.MainModule.FileVersionInfo.FileVersion;
+                    strMessage += "\n";
+                    strMessage += "File Size:" + p.MainModule.FileName;
+                    strMessage += "\n";
+                    strMessage += "User Name:" + Environment.UserDomainName.ToString() + "\\" + Environment.UserName;
+                    strMessage += "\n";
+                    strMessage += "Computer Name:" + Environment.MachineName;                    
+                    strMessage += "\n";
+                    strMessage += "Creation Time:" + a.CreationTime.ToLongTimeString();
+                    listB.SetToolTip(lstTasks, strMessage);
+                }//foreeach
+            }//try
+            catch (Exception ex)
+            {
+                strMessage = string.Format("Exception No 2. has occured , please try again or inform system administrator \n {0}", ex.Message);
+                MessageBox.Show(strMessage, strTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }//catch
+
+        }//lstTasks_Click
+
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lstTasks_Click(this, e);
+        }//propertiesToolStripMenuItem_Click
+
+        private void propertiesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Process[] allp = Process.GetProcesses();
+            string strMessage = "";
+            string strTitle = "Task manager program";
+            foreach (Process p in allp)
+            {
+
+                if (p.ProcessName.ToLower() == lstTasks.SelectedItem.ToString().ToLower())
+                {
+                    try
+                    {
+                        FileInfo a = new FileInfo(p.MainModule.FileName);
+                        string len = null;
+                        if (a.Length / 1073741824 > 0)
+                        {
+                            len = string.Format("{0:n2}", (double)a.Length / 1073741824 + " GB");
+                        }//if 
+                        else if (a.Length / 1048576 > 0)
+                        {
+                            len = string.Format("{0:n2}", (double)a.Length / 1048576 + " MB");
+                        }//else if
+                        else if (a.Length / 1024 > 0)
+                        {
+                            len = string.Format("{0:n2}", (double)a.Length / 1024 + " KB");
+                        }//else if
+                        else
+                        {
+                            len = a.Length + "Byte";
+                        }//else
+
+                        strMessage = "Process Name:" + p.ProcessName + "\nMem Usage:" + p.WorkingSet64 / 1024;
+                        strMessage += " K" + "\nProcess id:" + p.Id + "\nAddress :" + p.MainModule.FileName + "\nFile Name:";
+                        strMessage += p.MainModule.FileVersionInfo.OriginalFilename;
+                        strMessage += "\nFile Version:" + p.MainModule.FileVersionInfo.FileVersion;
+                        strMessage += "\nFile Size:" + len;
+                        strMessage += "\nCreation Time:" + a.CreationTime;
+                        strMessage += "\nUser Name:" + Environment.UserName;
+                        strMessage += "\nComputer Name:" + Environment.MachineName;
+
+                        MessageBox.Show(strMessage, strTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }//try
+                    catch (Exception ex)
+                    {
+                        strMessage = string.Format("Exception No 3. has occured , please try again or inform system administrator \n {0}", ex.Message);
+                        MessageBox.Show(strMessage, strTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }//catch
+                }//if
+            }//foreach
+        }//propertiesToolStripMenuItem1_Click
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            Load_Task_List();
+        }//btnRefresh_Click
+
+        //-----------------------------------------------------------------------------------------
+    }//public partial class frmTaskManger : Form
+}//miniTaskManager
